ed7578f4d3fed62846b52e137100c96511b60cfd
Andrew Tsai <a168tsai@gmail.com>
2022-08-24 09:34:08 -0600

    Pass second test

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,8 +1,20 @@
 class Venue
-  attr_reader :name, :capacity, :patrons 
+  attr_reader :name, :capacity, :patrons
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @patrons = []
+  end
+
+  def add_patron(name)
+    @patrons << (name)
+  end
+
+  def yell_at_patrons
+    names = []
+    @patrons.each do |patron|
+      names << patron.upcase
+    end
+    names 
   end
 end
